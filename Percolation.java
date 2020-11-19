ae1b18ca072a5d1ca2040a00c5e9ffba339dee74
proper-fish <aliona.kozushkina@gmail.com>
2020-11-20 01:01:26 +0300

    Percolation class done. percolates() method added + opens() method modified & fixed.

--- Percolation.java
+++ Percolation.java
@@ -1,7 +1,7 @@
 /* *****************************************************************************
  *  Name:              Aliona Kozushkina
  *  Coursera User ID:
- *  Last modified:     11/5/2020
+ *  Last modified:     11/19/2020
  **************************************************************************** */
 
 import edu.princeton.cs.algs4.WeightedQuickUnionUF;
@@ -17,10 +17,11 @@ public class Percolation {
     public Percolation(int n) {
         this.n = n;
         isInputValid(n);
-        ufArray = new WeightedQuickUnionUF(n * n - 1);
-        grid = new boolean[n][n];
-        for (int row = 0; row < n; row++) {
-            for (int col = 0; col < n; col++) {
+        ufArray = new WeightedQuickUnionUF(n * n + 2);
+        grid = new boolean[n + 1][n + 1];
+
+        for (int row = 0; row < n + 1; row++) {
+            for (int col = 0; col < n + 1; col++) {
                 grid[row][col] = false;
             }
         }
@@ -28,17 +29,17 @@ public class Percolation {
 
     // throws an exception if index is out of bounds
     private void isInputValid(int input) {
-        if (input < 0) throw new IllegalArgumentException("n should be 0 or larger");
+        if (input <= 0) throw new IllegalArgumentException("n should be 0 or larger");
     }
 
     private void isInputValid(int col, int row) {
-        if (row < 0 || row >= n) throw new IllegalArgumentException("row index out of bounds");
-        if (col < 0 || col >= n) throw new IllegalArgumentException("col index out of bounds");
+        if (row < 0 || row > n) throw new IllegalArgumentException("row index out of bounds");
+        if (col < 0 || col > n) throw new IllegalArgumentException("col index out of bounds");
     }
 
     // translates from 2D grid index to 1D UF data structure index
     private int xyTo1D(int x, int y) {
-        return (n * x + y);
+        return (n * (x - 1) + (y - 1));
     }
 
     // opens the site (row, col) if it is not open already
@@ -49,16 +50,23 @@ public class Percolation {
             openSites++;
         }
 
-        if (row > 0 && grid[row - 1][col]) {
+        if (row == 1) {
+            ufArray.union(n * n, xyTo1D(row, col));   // n * n is a virtual top site
+        }
+        if (row == n) {
+            ufArray.union(n * n + 1, xyTo1D(row, col));   // n * n + 1 is a virtual bottom site
+        }
+
+        if (row > 1 && grid[row - 1][col]) {
             ufArray.union(xyTo1D(row - 1, col), xyTo1D(row, col));
         }
-        if (row < 4 && grid[row + 1][col]) {
+        if (row < n && grid[row + 1][col]) {
             ufArray.union(xyTo1D(row + 1, col), xyTo1D(row, col));
         }
-        if (col > 0 && grid[row][col - 1]) {
+        if (col > 1 && grid[row][col - 1]) {
             ufArray.union(xyTo1D(row, col - 1), xyTo1D(row, col));
         }
-        if (col < 4 && grid[row][col + 1]) {
+        if (col < n && grid[row][col + 1]) {
             ufArray.union(xyTo1D(row, col + 1), xyTo1D(row, col));
         }
     }
@@ -72,13 +80,7 @@ public class Percolation {
     // is the site (row, col) full?
     public boolean isFull(int row, int col) {
         isInputValid(row, col);
-        boolean res = false;
-        for (int i = 0; i < n; i++) {
-            if (ufArray.find(xyTo1D(row, col)) == ufArray.find(xyTo1D(0, i))) {
-                res = true;
-            }
-        }
-        return res;
+        return ufArray.find(xyTo1D(row, col)) == ufArray.find(n * n);
     }
 
     // returns the number of open sites
@@ -88,13 +90,13 @@ public class Percolation {
 
     // does the system percolate?
     public boolean percolates() {
-        return true;
+        return ufArray.find(n * n + 1) == ufArray.find(n * n);
     }
 
     // prints the grid
     private void visualize(Percolation percolation) {
-        for (int i = 0; i < 5; i++) {
-            for (int j = 0; j < 5; j++) {
+        for (int i = 1; i <= n; i++) {
+            for (int j = 1; j <= n; j++) {
                 String res = (percolation.grid[i][j]) ? "⚪️" : "⚫️️";
                 System.out.printf("%s ", res);
             }
@@ -105,20 +107,7 @@ public class Percolation {
 
     // test client
     public static void main(String[] args) {
-        Percolation percolation = new Percolation(5);
-        percolation.visualize(percolation);
-        percolation.open(2, 3);
-        percolation.visualize(percolation);
-        System.out.printf("The site is full: %s \n \n", percolation.isFull(2, 3));
-        percolation.open(2, 2);
-        percolation.visualize(percolation);
-        System.out.printf("The site is full: %s \n \n", percolation.isFull(2, 2));
-        percolation.open(1, 3);
-        percolation.visualize(percolation);
-        System.out.printf("The site is full: %s \n \n", percolation.isFull(1, 3));
-        percolation.open(0, 3);
-        percolation.visualize(percolation);
-        System.out.printf("The site is full: %s \n \n", percolation.isFull(0, 3));
-        System.out.printf("Number of open sites: %s \n \n", percolation.numberOfOpenSites());
+        // Percolation percolation = new Percolation(5);
+        // percolation.visualize(percolation);
     }
 }
